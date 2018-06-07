6ebcc927d96e1ed93f1b41c09eece6cb6be4ae91
Jagruti <jagruti@drive-mac.local>
2018-06-07 14:53:03 +0530

    Initial Commit

---
+++ Dialer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Dialer
+//
+//  Created by Jagruti on 6/7/18.
+//  Copyright © 2018 Jagruti. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
